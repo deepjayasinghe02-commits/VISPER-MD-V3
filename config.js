530cf6356f1bdcb33eb4494198150fb6787d2540
deepjayasinghe02-commits <deepjayasinghe02@gmail.com>
2025-08-29 10:37:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&btIy0R6b#6EOpNsLTl96nyE_qsoBT6cl4qmAgUDHzBMCs9mhrJRM' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
-SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'deepjayasinghe02-commits' : process.env.SESSION_NAME, 
 
 };
 
